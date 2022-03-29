c994c8778becd7f4aedf3d8aa9f5eb3680f6815d
delexii <delia.bute@yahoo.com>
2022-03-29 17:09:31 +0100

    configured producer callbacks and sticky partitioning

---
+++ kafka-basics/src/main/java/io/conductor/demos/kafka/ProducerDemoWithCallback.java
@@ -0,0 +1,66 @@
+package io.conductor.demos.kafka;
+
+import org.apache.kafka.clients.producer.*;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Properties;
+
+public class ProducerDemoWithCallback {
+
+    private static final Logger log = LoggerFactory.getLogger(ProducerDemoWithCallback.class.getSimpleName());
+
+    public static void main(String[] args) {
+        log.info("I am a Kafka Producer");
+
+        // create Producer Properties
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        // create the Producer
+        KafkaProducer<String,String> producer = new KafkaProducer<>(properties);
+
+
+        for (int i = 0 ; i < 10; i++){
+            // create a producer record
+            ProducerRecord<String, String> producerRecord = new ProducerRecord<>("demo_java", "hello world " + i );
+
+            // send data - asynchronous operation
+
+            producer.send(producerRecord, new Callback() {
+                @Override
+                public void onCompletion(RecordMetadata metadata, Exception exception) {
+                    // executes every time a record is successfully sent or on exception is thrown
+                    if (exception == null) {
+                        // the record was successfully sent
+                        log.info("Received new metadata \n" +
+                                "Topic: " + metadata.topic() + "\n" +
+                                "Partition: " + metadata.partition() + "\n" +
+                                "Offset: " + metadata.offset() + "\n" +
+                                "Timestamp: " + metadata.timestamp());
+                    } else {
+                        log.error("Error while producing", exception);
+                    }
+                }
+            });
+
+            // highly inefficient, but time between each message produced can send messages to diff partitions
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e){
+                e.printStackTrace();
+            }
+        }
+
+        // flush data - synchronous operation
+        producer.flush();
+
+        // flush and close producer
+        producer.close();
+    }
+
+
+}
